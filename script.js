c68d25ec4f5ffe07c8492bd8ce385bfd3de8ba8a
linix2027 <noahgodwin334@gmail.com>
2024-08-19 12:58:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let yourage = 25;
+
+if(yourage==23){
+console.log("you are old ,,,");
+}
+else if(yourage==25){
+    console.log("your about to make it  "); 
+}
+else{
+    console.log("you are not");
+}
